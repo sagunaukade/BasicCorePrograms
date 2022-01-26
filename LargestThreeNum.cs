7da564c5292a2aa778b726289fe5a2a2642d6373
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-01-27 00:35:27 +0530

    Adding the Largest Among Three Numbers

---
+++ LargestThreeNum.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class LargestThreeNum
+    {
+        static void LargestAmongThreeNUm()
+        {
+            int num1, num2, num3;
+            Console.WriteLine("Enter First Number");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second Number");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter third Number");
+            num3 = Convert.ToInt32(Console.ReadLine());
+            if (num1 > num2)
+            {
+                if (num1 > num3)
+                {
+                    Console.WriteLine("First number is greatest among three");
+                }
+                else
+                {
+                    Console.WriteLine("Third number is greatest among three");
+
+                }
+
+            }
+            else if (num2 > num3)
+            {
+                Console.WriteLine("Second number is greatest among three");
+
+            }
+            else
+            {
+                Console.WriteLine("Third number is greatest among three");
+
+            }
+        }
+    }
+}
