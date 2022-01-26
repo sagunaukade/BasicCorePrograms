cd3a81faae0680c7c51021cd9d43ddf207c60ed2
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-01-27 00:37:59 +0530

    Adding First Program

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n Welcome to the basic core programs in c sharp");
+            Console.ReadLine();
+        }
+    }
+}
