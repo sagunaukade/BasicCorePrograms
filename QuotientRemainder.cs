8c7acdbc16f4e2b7c1c852672d8d30e7709a4412
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-01-27 00:37:02 +0530

    Adding Quotient and Remainder Program

---
+++ QuotientRemainder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class QuotientRemainder
+    {
+        static void QuoRemain()
+        {
+            int dividend = 40, divisior = 2;
+            int quotient = dividend / divisior;
+            int remainder = dividend % divisior;
+            Console.WriteLine("\n Dividend {0} Divisor {1}", dividend, divisior);
+            Console.WriteLine("\n Quotient = " + quotient);
+            Console.WriteLine("\n Remainder = " + remainder);
+            Console.ReadLine();
+        }
+    }
+}
