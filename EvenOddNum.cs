3ba43fc43010687f757c0f6dc7cb210e06d597bb
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-01-27 00:33:18 +0530

    Adding Even And Odd Number Program

---
+++ EvenOddNum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class EvenOddNum
+    {
+       static void EvenOdd()
+        {
+            int num, rem;
+            Console.WriteLine("\n Check number is even or odd ");
+            Console.WriteLine("\n Enter Number ");
+            num = Convert.ToInt32(Console.ReadLine());
+            rem = num / 2;
+            if(rem==0)
+            {
+                Console.WriteLine("\n {0} is even number " + num);
+
+            }
+            else
+            {
+                Console.WriteLine("\n {0} is odd number " + num);
+            }
+        }
+    }
+}
