ba6dc7a9e58e1214866a4ee7347644283cc7d7fa
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-01-27 00:39:40 +0530

    Adding Vowel And Consonants Program

---
+++ VowelConsonants.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class VowelConsonants
+    {
+        static void VowelConso()
+        {
+            char ch;
+            bool lower_vowel, upper_vowel;
+            Console.WriteLine("Enter character\n ");
+            ch = Convert.ToChar(Console.ReadLine());
+            lower_vowel = (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
+            upper_vowel = (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U');
+            if(lower_vowel || upper_vowel)
+            {
+                Console.WriteLine("Character is vowel ");
+            }
+            else
+            {
+                Console.WriteLine("Character is consonant ");
+
+            }
+        }
+    }
+}
