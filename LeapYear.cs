dc6498637c3306886b84cfe12033200d5e28d4f6
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-01-27 01:42:52 +0530

    Adding Leap Year Program

---
+++ LeapYear.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class LeapYear
+    {
+        public static void LeapOrNot()
+        {
+            int chk_year;
+            Console.Write("Check whether a given year is leap year or not\n");
+            Console.Write("Enter the year ");
+            chk_year = Convert.ToInt32(Console.ReadLine());
+            if ((chk_year % 400) == 0)
+                Console.WriteLine("{0} is a leap year.\n", chk_year);
+            else if ((chk_year % 100) == 0)
+                Console.WriteLine("{0} is not a leap year.\n", chk_year);
+            else if ((chk_year % 4) == 0)
+                Console.WriteLine("{0} is a leap year.\n", chk_year);
+            else
+                Console.WriteLine("{0} is not a leap year.\n", chk_year);
+        }
+    }
+}
+
