298e892df15811d18c2dab1e266dcf3a47716380
sagunaukade <53419654+sagunaukade@users.noreply.github.com>
2022-01-27 11:07:02 +0530

    Adding Flip Coin Program

---
+++ FlipCoin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgramsInC_Sharp
+{
+    internal class FlipCoin
+    {
+        //public void Welcome()
+       // {
+          //  Console.WriteLine("\n hello csharp");
+       // }
+    }
+}
